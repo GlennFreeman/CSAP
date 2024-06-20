cd944cc2cff7c952e8aaf31cc5604e8f163bb907
GlennFreeman <glenn.freeman.business@gmail.com>
2024-06-20 16:14:16 -0400

    more tab stuff

--- dashboard.py
+++ dashboard.py
@@ -1,32 +1,29 @@
 import sqlite3
 import dearpygui.dearpygui as dpg
 
-def stringify_tuple(x):
-    return str(x)[2:-3].title()
-
 def button_callback(sender, app_data, user_data):
     print(f"sender is: {sender}")
     print(f"app_data is: {app_data}")
     print(f"user_data is: {user_data}")
-    dpg.add_tab(label=stringify_tuple(user_data), parent="tab bar")
+    dpg.add_tab(label=(user_data[0].title()), parent="tab bar", closable=True)
 
 
 con = sqlite3.connect("app.db")
 cur = con.cursor()
-sql = "SELECT topic FROM scores ORDER BY score DESC"
+sql = "SELECT topic, score FROM scores ORDER BY score DESC"
 res = cur.execute(sql)
 topics = res.fetchall()
 
 dpg.create_context()
 
 with dpg.window(tag="Primary Window"):
-    dpg.add_tab_bar(tag="tab bar")
-    with dpg.tab(label="main", parent="tab bar"):          
-            buttons = []
-            longest = 0
-            for x in range(10):
-                label = stringify_tuple(topics[x])
-                buttons.append(dpg.add_button(label=label, user_data=topics[x], callback=button_callback))
+    with dpg.tab_bar(tag="tab bar", reorderable=True):
+        with dpg.tab(label="main", parent="tab bar", closable=False, order_mode=dpg.mvTabOrder_Leading):          
+                buttons = []
+                longest = 0
+                for x in range(10):
+                    label = f'{x+1:>2}. {topics[x][0].title():<30}{topics[x][1]:>5.2f}'
+                    buttons.append(dpg.add_button(label=label, user_data=topics[x], callback=button_callback))
 
 dpg.create_viewport(title="CSAP")
 dpg.setup_dearpygui()
