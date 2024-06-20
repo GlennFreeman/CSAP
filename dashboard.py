995fa10e6c777ec1d64393ff251f160ee3fd5cd5
GlennFreeman <glenn.freeman.business@gmail.com>
2024-06-20 13:28:18 -0400

    started playing with dear pygui to make dashboard

---
+++ dashboard.py
@@ -0,0 +1,40 @@
+import sqlite3
+import dearpygui.dearpygui as dpg
+
+def stringify_tuple(x):
+    return str(x)[2:-3].title()
+
+def button_callback(sender, app_data, user_data):
+    print(f"sender is: {sender}")
+    print(f"app_data is: {app_data}")
+    print(f"user_data is: {user_data}")
+    dpg.add_tab(label=stringify_tuple(user_data), parent="tab bar")
+
+
+con = sqlite3.connect("app.db")
+cur = con.cursor()
+sql = "SELECT topic FROM scores ORDER BY score DESC"
+res = cur.execute(sql)
+topics = res.fetchall()
+
+dpg.create_context()
+
+
+
+with dpg.window(tag="Primary Window"):
+    dpg.add_tab_bar(tag="tab bar")
+    dpg.add_tab(label="main", parent="tab bar")
+    dpg.add_text("Hello, world")
+    
+    buttons = []
+    longest = 0
+    for x in range(10):
+        label = stringify_tuple(topics[x])
+        buttons.append(dpg.add_button(label=label, user_data=topics[x], callback=button_callback))
+
+dpg.create_viewport(title="CSAP")
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.set_primary_window("Primary Window", True)
+dpg.start_dearpygui()
+dpg.destroy_context()   
